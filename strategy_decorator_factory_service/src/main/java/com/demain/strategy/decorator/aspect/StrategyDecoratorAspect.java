9c7b6eeb1eb9bcec178b8dbea5b78350b953df68
demain_lee <lmm_work@163.com>
2024-07-25 15:34:50 +0800

    :art: 自动注入每个策略和装饰器的构造函数，避免硬编码

--- strategy_decorator_factory_service/src/main/java/com/demain/strategy/decorator/aspect/StrategyDecoratorAspect.java
+++ strategy_decorator_factory_service/src/main/java/com/demain/strategy/decorator/aspect/StrategyDecoratorAspect.java
@@ -1,47 +1,47 @@
-package com.demain.strategy.decorator.aspect;
-
-import com.demain.strategy.decorator.annotation.Decoratable;
-import com.demain.strategy.decorator.c.decorator.StrategyDecorator;
-import com.demain.strategy.decorator.service.IndexService;
-import com.demain.strategy.decorator.strategy.QsnStrategy;
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.stereotype.Component;
-
-import java.lang.reflect.Constructor;
-
-@Aspect
-@Component
-public class StrategyDecoratorAspect {
-
-    @Autowired
-    private ApplicationContext applicationContext;
-
-    @Pointcut("execution(* com.demain.strategy.decorator.strategy.QsnStrategyFactory.getStrategy(..))")
-    public void getStrategyPointcut() {}
-
-    @Around("getStrategyPointcut()")
-    public Object aroundGetStrategy(ProceedingJoinPoint joinPoint) throws Throwable {
-        QsnStrategy strategy = (QsnStrategy) joinPoint.proceed();
-        Decoratable decoratable = strategy.getClass().getAnnotation(Decoratable.class);
-        if (decoratable != null) {
-            Class<? extends StrategyDecorator> decoratorClass = decoratable.decorator();
-            try {
-                Constructor<? extends StrategyDecorator> constructor = decoratorClass.getConstructor(QsnStrategy.class, IndexService.class);
-                IndexService indexService = applicationContext.getBean(IndexService.class);
-                StrategyDecorator decorator = constructor.newInstance(strategy, indexService);
-                applicationContext.getAutowireCapableBeanFactory().autowireBean(decorator);
-                return decorator;
-            } catch (NoSuchMethodException e) {
-                throw new RuntimeException("无法找到适当的构造函数: " + decoratorClass.getName(), e);
-            } catch (Exception e) {
-                throw new RuntimeException("无法创建装饰器实例: " + decoratorClass.getName(), e);
-            }
-        }
-        return strategy;
-    }
-}
+//package com.demain.strategy.decorator.aspect;
+//
+//import com.demain.strategy.decorator.annotation.Decoratable;
+//import com.demain.strategy.decorator.c.decorator.StrategyDecorator;
+//import com.demain.strategy.decorator.service.IndexService;
+//import com.demain.strategy.decorator.strategy.QsnStrategy;
+//import org.aspectj.lang.ProceedingJoinPoint;
+//import org.aspectj.lang.annotation.Around;
+//import org.aspectj.lang.annotation.Aspect;
+//import org.aspectj.lang.annotation.Pointcut;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.stereotype.Component;
+//
+//import java.lang.reflect.Constructor;
+//
+//@Aspect
+//@Component
+//public class StrategyDecoratorAspect {
+//
+//    @Autowired
+//    private ApplicationContext applicationContext;
+//
+//    @Pointcut("execution(* com.demain.strategy.decorator.strategy.QsnStrategyFactory.getStrategy(..))")
+//    public void getStrategyPointcut() {}
+//
+//    @Around("getStrategyPointcut()")
+//    public Object aroundGetStrategy(ProceedingJoinPoint joinPoint) throws Throwable {
+//        QsnStrategy strategy = (QsnStrategy) joinPoint.proceed();
+//        Decoratable decoratable = strategy.getClass().getAnnotation(Decoratable.class);
+//        if (decoratable != null) {
+//            Class<? extends StrategyDecorator> decoratorClass = decoratable.decorator();
+//            try {
+//                Constructor<? extends StrategyDecorator> constructor = decoratorClass.getConstructor(QsnStrategy.class, IndexService.class);
+//                IndexService indexService = applicationContext.getBean(IndexService.class);
+//                StrategyDecorator decorator = constructor.newInstance(strategy, indexService);
+//                applicationContext.getAutowireCapableBeanFactory().autowireBean(decorator);
+//                return decorator;
+//            } catch (NoSuchMethodException e) {
+//                throw new RuntimeException("无法找到适当的构造函数: " + decoratorClass.getName(), e);
+//            } catch (Exception e) {
+//                throw new RuntimeException("无法创建装饰器实例: " + decoratorClass.getName(), e);
+//            }
+//        }
+//        return strategy;
+//    }
+//}
--- strategy_decorator_factory_service/src/main/java/com/demain/strategy/decorator/strategy/StrategyDecoratorPostProcessor.java
+++ strategy_decorator_factory_service/src/main/java/com/demain/strategy/decorator/strategy/StrategyDecoratorPostProcessor.java
@@ -1,51 +1,91 @@
-//package com.demain.strategy.decorator.strategy;
-//
-//import com.demain.strategy.decorator.annotation.Decoratable;
-//import com.demain.strategy.decorator.c.decorator.StrategyDecorator;
-//import com.demain.strategy.decorator.service.IndexService;
-//import io.micrometer.common.lang.Nullable;
-//import org.springframework.beans.BeansException;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.config.BeanPostProcessor;
-//import org.springframework.context.ApplicationContext;
-//import org.springframework.stereotype.Component;
-//
-//import java.lang.reflect.Constructor;
-//
-///**
-// * BeanPostProcessor 允许在 Spring 容器完成初始化之后对 bean 进行进一步处理。
-// */
-//@Component
-//public class StrategyDecoratorPostProcessor implements BeanPostProcessor {
-//
-//    @Autowired
-//    private ApplicationContext applicationContext;
-//
-//    @Override
-//    public Object postProcessBeforeInitialization(@Nullable Object bean,@Nullable String beanName) throws BeansException {
-//        return bean;
-//    }
-//
-//    @Override
-//    public Object postProcessAfterInitialization(@Nullable Object bean,@Nullable String beanName) throws BeansException {
-//        if (bean instanceof QsnStrategy) {
-//            Decoratable decoratable = bean.getClass().getAnnotation(Decoratable.class);
-//            if (decoratable != null) {
-//                Class<? extends StrategyDecorator> decoratorClass = decoratable.decorator();
-//                try {
-//                    // 确保这里的构造函数参数与装饰器类的构造函数匹配
-//                    Constructor<? extends StrategyDecorator> constructor = decoratorClass.getConstructor(QsnStrategy.class, IndexService.class);
-//                    IndexService indexService = applicationContext.getBean(IndexService.class);
-//                    StrategyDecorator decorator = constructor.newInstance(bean, indexService);
-//                    // 自动注入装饰器的依赖
-//                    applicationContext.getAutowireCapableBeanFactory().autowireBean(decorator);
-//                    return decorator;
-//                } catch (Exception e) {
-//                    throw new RuntimeException("无法创建装饰器实例: " + decoratorClass.getName(), e);
-//                }
-//            }
-//        }
-//        return bean;
-//    }
-//}
-//
+package com.demain.strategy.decorator.strategy;
+
+import com.demain.strategy.decorator.annotation.Decoratable;
+import com.demain.strategy.decorator.c.decorator.StrategyDecorator;
+import io.micrometer.common.lang.Nullable;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * BeanPostProcessor 允许在 Spring 容器完成初始化之后对 bean 进行进一步处理。
+ */
+@Component
+public class StrategyDecoratorPostProcessor implements BeanPostProcessor {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Override
+    public Object postProcessBeforeInitialization(@Nullable Object bean, @Nullable String beanName) throws BeansException {
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(@Nullable Object bean, @Nullable String beanName) throws BeansException {
+        if (bean instanceof QsnStrategy) {
+            return processQsnStrategy((QsnStrategy) bean);
+        }
+        return bean;
+    }
+
+    private Object processQsnStrategy(QsnStrategy strategy) {
+        Decoratable decoratable = strategy.getClass().getAnnotation(Decoratable.class);
+        if (decoratable != null) {
+            Class<? extends StrategyDecorator> decoratorClass = decoratable.decorator();
+            try {
+                return createDecorator(strategy, decoratorClass);
+            } catch (Exception e) {
+                throw new RuntimeException("无法创建装饰器实例: " + decoratorClass.getName(), e);
+            }
+        }
+        return strategy;
+    }
+
+    private StrategyDecorator createDecorator(QsnStrategy strategy, Class<? extends StrategyDecorator> decoratorClass) throws Exception {
+        Constructor<?>[] constructors = decoratorClass.getConstructors();
+        for (Constructor<?> constructor : constructors) {
+            if (isSuitableConstructor(constructor)) {
+                Object[] args = resolveConstructorArguments(constructor.getParameterTypes(), strategy);
+                StrategyDecorator decorator = (StrategyDecorator) constructor.newInstance(args);
+                applicationContext.getAutowireCapableBeanFactory().autowireBean(decorator);
+                return decorator;
+            }
+        }
+        throw new RuntimeException("没有找到合适的构造函数: " + decoratorClass.getName());
+    }
+
+    private boolean isSuitableConstructor(Constructor<?> constructor) {
+        Class<?>[] parameterTypes = constructor.getParameterTypes();
+        boolean hasStrategy = false;
+        for (Class<?> paramType : parameterTypes) {
+            if (QsnStrategy.class.isAssignableFrom(paramType)) {
+                hasStrategy = true;
+            } else {
+                try {
+                    applicationContext.getBean(paramType);
+                } catch (BeansException e) {
+                    return false; // 如果bean不存在则不适用此构造函数
+                }
+            }
+        }
+        return hasStrategy;
+    }
+
+    private Object[] resolveConstructorArguments(Class<?>[] parameterTypes, QsnStrategy strategy) {
+        Object[] args = new Object[parameterTypes.length];
+        for (int i = 0; i < parameterTypes.length; i++) {
+            if (QsnStrategy.class.isAssignableFrom(parameterTypes[i])) {
+                args[i] = strategy;
+            } else {
+                args[i] = applicationContext.getBean(parameterTypes[i]);
+            }
+        }
+        return args;
+    }
+}
+
